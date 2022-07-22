5a7e05b1e144ce9504735d84afd1b5b2909eca88
Red Snyper <zbon77@gmail.com>
2022-07-22 13:56:00 +0545

    project requirement packages

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from app.database import add_models_to_database,generate_fake_data
 from app.api import api_router
+from app.api.endpoints import login
 app = FastAPI()
 add_models_to_database()
 generate_fake_data()
 
 app.include_router(api_router, prefix='/api')
-
+app.include_router(login.router)
 
 @app.get('/')
 def main():
